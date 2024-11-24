2bd3de2678932cf87c82f5425f1e5e36a9d69833
Ott Saluvere <ott.saluvere@icloud.com>
2024-11-24 14:59:37 +0000

    Experimenting with GOP and created a BMP to UEFI buffer script.

---
+++ convert_bmp.py
@@ -0,0 +1,103 @@
+import sys
+
+def get_bytes_per_pixel(bmp_file):
+    bmp_file.seek(28)
+
+    return int.from_bytes(bmp_file.read(2)[::-1]) // 8
+
+def get_pixel_array_offset(bmp_file):
+    bmp_file.seek(10)
+
+    return int.from_bytes(bmp_file.read(4)[::-1])
+
+def uses_color_pallet(bmp_file):
+    bmp_file.seek(46)
+    
+    return int.from_bytes(bmp_file.read(4)[::-1])
+
+def calculate_padding(bmp_file):
+    width, _ = get_image_dimensions(bmp_file)
+    bytes_per_pixel = get_bytes_per_pixel(bmp_file)
+
+    padding = (bytes_per_pixel * width) % 4
+
+    if padding == 0:
+        return 0
+    
+    padding = 4 - padding 
+
+    return padding
+
+def get_image_dimensions(bmp_file):
+    out = [0, 0]
+
+    bmp_file.seek(18)
+    out[0] = int.from_bytes(bmp_file.read(4)[::-1])
+
+    bmp_file.seek(22)
+    out[1] = int.from_bytes(bmp_file.read(4)[::-1])
+
+    return tuple(out)
+
+def check_DIB_header(bmp_file):
+    bmp_file.seek(14)
+
+    header_size = int.from_bytes(bmp_file.read(4)[::-1])
+
+    if (header_size != 124):
+        print("This script only allows BITMAPV5HEADER.")
+        exit(1)
+
+def parse_bmp_pixel_data(bmp_f):
+    bmp_pixel_rows = []
+
+    width, height = get_image_dimensions(bmp_f)
+    bytes_per_px = get_bytes_per_pixel(bmp_f)
+    padding = calculate_padding(bmp_f)
+
+    bmp_f.seek(get_pixel_array_offset(bmp_f))
+
+    for _ in range(height):
+        row = bytearray()
+
+        for _ in range(width):
+            pixel = bmp_f.read(bytes_per_px)
+
+            row.extend(pixel)
+            row.extend(b'\x00')
+        
+        bmp_f.seek(padding, 1)
+        bmp_pixel_rows.append(row)
+    
+    return bmp_pixel_rows
+
+def main(args):
+    if (len(args) != 2):
+        print("Need a file to convert.")
+
+    output_filename = f'{args[1].split('.')[0]}.raw'
+
+    reverse_rows = None
+
+    with open(args[1], 'rb') as bmp_f:
+        check_DIB_header(bmp_f)
+
+        if (get_bytes_per_pixel(bmp_f) != 3):
+            print("BMP encoding need 24 bits per pixel.")
+            exit(1)
+
+        if (uses_color_pallet(bmp_f)):
+            print("BMP color pallet not supported.")
+            exit(1)
+
+        print(calculate_padding(bmp_f))
+
+        reverse_rows = parse_bmp_pixel_data(bmp_f)
+    
+    with open(output_filename, 'wb') as out_f:
+        for row in reversed(reverse_rows):
+            out_f.write(row)
+            print(row)
+
+if __name__ == '__main__':
+    main(sys.argv)
--- main.c
+++ main.c
@@ -1,13 +1,80 @@
 #include <efi.h>
 #include <efilib.h>
 
-EFI_STATUS
-EFIAPI
-efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
-{
+EFI_GUID gEfiSimplePointerProtocolGuid = EFI_SIMPLE_POINTER_PROTOCOL_GUID;
+
+EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+    EFI_SIMPLE_POINTER_PROTOCOL *PointerProtocol;
+    EFI_GRAPHICS_OUTPUT_PROTOCOL *GraphicsProtocol;
+    EFI_SIMPLE_POINTER_STATE PointerState;
+    EFI_GRAPHICS_OUTPUT_MODE_INFORMATION *ModeInfo;
+    EFI_GRAPHICS_OUTPUT_BLT_PIXEL RedPixel = {0x00, 0xFF, 0xFF, 0x00};
+    EFI_GRAPHICS_OUTPUT_BLT_PIXEL BlackPixel = {0x00, 0x00, 0x00, 0x00};
+    UINT32 ScreenWidth, ScreenHeight;
+    UINTN RedSquareSize = 20;
+    INT32 MouseX = 0, MouseY = 0;
+
     InitializeLib(ImageHandle, SystemTable);
 
-    Print(L"Hello, world!\n");
+    if (uefi_call_wrapper(BS->LocateProtocol, 3, &gEfiGraphicsOutputProtocolGuid, NULL, (void **)&GraphicsProtocol) != EFI_SUCCESS) {
+        Print(L"Failed to locate Graphics Output Protocol.\n");
+        return EFI_UNSUPPORTED;
+    }
+
+    ModeInfo = GraphicsProtocol->Mode->Info;
+    ScreenWidth = ModeInfo->HorizontalResolution;
+    ScreenHeight = ModeInfo->VerticalResolution;
+
+    if (uefi_call_wrapper(BS->LocateProtocol, 3, &gEfiSimplePointerProtocolGuid, NULL, (void **)&PointerProtocol) != EFI_SUCCESS) {
+        Print(L"Failed to locate Simple Pointer Protocol.\n");
+        return EFI_UNSUPPORTED;
+    }
+
+    uefi_call_wrapper(PointerProtocol->Reset, 2, PointerProtocol, TRUE);
+    
+    while (1) {
+        if (uefi_call_wrapper(PointerProtocol->GetState, 2, PointerProtocol, &PointerState) == EFI_SUCCESS) {
+            MouseX += (INT32)(PointerState.RelativeMovementX);
+            MouseY += (INT32)(PointerState.RelativeMovementY);
+
+            if (MouseX < 0) MouseX = 0;
+            if (MouseX >= (INT32)ScreenWidth) MouseX = ScreenWidth - 1;
+            if (MouseY < 0) MouseY = 0;
+            if (MouseY >= (INT32)ScreenHeight) MouseY = ScreenHeight - 1;
+
+            if (PointerState.LeftButton) {
+                uefi_call_wrapper(GraphicsProtocol->Blt, 10,
+                                  GraphicsProtocol,
+                                  &RedPixel,
+                                  EfiBltVideoFill,
+                                  0, 0,
+                                  MouseX, MouseY,
+                                  RedSquareSize, RedSquareSize,
+                                  0
+                );
+            }
+        }
+
+        EFI_INPUT_KEY Key;
+
+        if (uefi_call_wrapper(SystemTable->ConIn->ReadKeyStroke, 2, SystemTable->ConIn, &Key) == EFI_SUCCESS) {
+            if (Key.UnicodeChar == L'c') {
+                uefi_call_wrapper(GraphicsProtocol->Blt, 10,
+                                  GraphicsProtocol,
+                                  &BlackPixel,
+                                  EfiBltVideoFill,
+                                  0, 0,
+                                  0, 0,
+                                  GraphicsProtocol->Mode->Info->HorizontalResolution, GraphicsProtocol->Mode->Info->VerticalResolution,
+                                  0
+                );
+            } else if (Key.UnicodeChar == L'q') {
+                break;
+            }
+        }
+
+        uefi_call_wrapper(BS->Stall, 1, 5000);
+    }
 
     return EFI_SUCCESS;
 }
